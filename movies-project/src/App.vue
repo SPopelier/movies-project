98b9a86a28a6eb3814b5d242c9f02063665b755e
Sarah Popelier <sarah.popelier@hotmail.fr>
2025-12-09 16:27:06 +0100

    second commit

---
+++ movies-project/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { axiosRequest } from './requeteAPI/apiRequest.js'
+import {onMounted} from "vue";
+import axios from "axios";
+
+onMounted(() => {
+  axiosRequest();
+})
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
